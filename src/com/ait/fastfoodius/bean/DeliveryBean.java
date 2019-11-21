a553487f3006d18356ed71c24fcf442c1048936d
HP <a00277167@student.ait.ie>
2019-11-21 14:23:29 +0200

    deliverydao ready

--- src/com/ait/fastfoodius/bean/DeliveryBean.java
+++ src/com/ait/fastfoodius/bean/DeliveryBean.java
@@ -1,15 +1,42 @@
 package com.ait.fastfoodius.bean;
 
 public class DeliveryBean {
-	
-	/*delivery_id INTEGER NOT NULL,
-      order_ID  INTEGER NOT NULL,
-      deliveredBy  VARCHAR (30) NOT NULL,
-      deliveryStatus VARCHAR (30) NOT NULL,
-      whenDelivered */
 
 	private int deliveryid;
-	int 
+	private int order_ID;
+	private String deliveredby;
+	private String deliveryStatus;
+	private String whenDelivered;
 	
-	
+	public int getdeliveryid() {
+		return deliveryid;	
+	}
+	public void setdeliveryid(int deliveryid) {
+		this.deliveryid = deliveryid;
+	}
+	public int getorder_ID() {
+		return order_ID;
+	}
+	public void setorder_ID(int order_ID) {
+		this.order_ID = order_ID;
+	}
+	public String getdeliveredby() {
+		return deliveredby;
+	}
+	public void setdeliveredby(String deliveredby) {
+		this.deliveredby = deliveredby;
+	}
+	public String getdeliveryStatus() {
+		return deliveryStatus;
+	}
+	public void setdeliveryStatus(String deliveryStatus) {
+		this.deliveryStatus= deliveryStatus;
+		
+	}
+	public String getwhenDelivered() {
+		return whenDelivered;
+	}
+	public void setwhenDelivered(String whenDelivered) {
+		this.whenDelivered = whenDelivered;
+	}
 }
