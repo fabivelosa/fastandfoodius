2125056632e73d290a90cfb5b72e76f6fecd8312
FabiVelosa <fabivelosa@gmail.com>
2019-11-23 07:55:18 +0000

    menu controller

--- WebContent/pages/forgotpassword.jsp
+++ WebContent/pages/forgotpassword.jsp
@@ -1,24 +1,37 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1" 	
-	import ="java.util.*" %>
-
 <!DOCTYPE html>
 <html lang="en">
 
-
-
-
 <head>
-	
+
+<title>Access Denied</title>
+
+<%@include file="../includes/header.jsp" %>
+
+
 </head>
-<body id="page-top" onLoad="highlight_menu('#login','#fed136'),highlight_menu('#contact','#fff')">
+
+<body id="page-top">
+
+	<!-- Navigation -->
+	<!-- Navigation -->
+	<%@include file="../includes/navbar_menu.jsp" %>
 	
-    
+	<!-- Header -->
+	<header class="inthead">
 		<div class="container">
-			<div class="intro-heading text-uppercase"> Please call to our DataBaseAdmin Oladejo! <br> 
-			He will reset your password in a second!</div>
-			
+			<div class="intro-text">
+				<div class="intro-lead-in"> Did you forget your password? No problem at all! </div>
+				
+				<div class="intro-heading text-uppercase"> Call us!  </div>
+				<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+					href="#services">090 - 199777</a>
+
 		</div>
+	</header>
+
+
+<%@include file="../includes/footer.jsp" %>
+
 </body>
-	
+
 </html>
---
+++ src/com/ait/fastfoodius/servlets/Menu.java
@@ -0,0 +1,50 @@
+package com.ait.fastfoodius.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ait.fastfoodius.bean.MenuBean;
+
+/**
+ * Servlet implementation class Menu
+ */
+@WebServlet("/Menu")
+public class Menu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Menu() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//access MEnu DAO
+		List<MenuBean> products = null;
+		
+		request.setAttribute("products",products );
+		String contextPath = request.getContextPath();
+		response.sendRedirect(contextPath+"/pages/menu.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
